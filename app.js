5cb9149fe3423ae2be96a5d8758b6b35ff265714
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-02-14 14:58:11 +0000

    Refactor EHMFs into errors file

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 const express = require('express');
 const { getArticleById } = require('./controllers/articles.controller');
 const { getTopics } = require('./controllers/topics.controller');
+const { handleCustomErrors, handlePSQLErrors } = require('./errors');
 
 const app = express();
 
 app.get('/api/topics', getTopics);
 app.get('/api/articles/:article_id', getArticleById);
 
-app.use((err, req, res, next) => {
-    if (err.status && err.message) {
-        res.status(err.status).send({ message: err.message });
-      }
-    else next(err);
-})
-
-app.use((err, req, res, next) => {
-    if (err.code === '22P02') {
-        res.status(400).send({ message: "Bad request" });
-    }
-})
-
 app.all('/*', (req, res) => {
     res.status(404).send({ message: "Path not found" });
-})
+});
+
+app.use(handleCustomErrors);
+app.use(handlePSQLErrors);
 
 module.exports = app;
