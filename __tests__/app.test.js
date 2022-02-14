cc097e131e52050fba49e5c7c86f03d09ef6a292
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-02-14 11:54:45 +0000

    Add GET /api/topics endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,3 +1,39 @@
 const app = require('../app');
 const request = require('supertest');
+const seed = require('../db/seeds/seed');
+const data = require('../db/data/test-data');
+const db = require('../db/connection');
 
+seed(data).then(() => db.end());
+
+describe('app', () => {
+    describe('GET /api/topics', () => {
+        test('status:200 - responds with an array of topic objects', () => {
+            return request(app)
+                .get('/api/topics')
+                .expect(200)
+                .then(({ body: { topics } }) => {
+                    expect(topics).toBeInstanceOf(Array);
+                    expect(topics).toHaveLength(3);
+                    topics.forEach(topic => {
+                        expect(topic).toEqual(
+                            expect.objectContaining({
+                                slug: expect.any(String),
+                                description: expect.any(String)
+                            })
+                        )
+                    })
+                })
+        });
+        test('status:404 - responds with "Path not found"', () => {
+            return request(app)
+                .get('/api/not-a-path')
+                .expect(404)
+                .then(({ body }) => {
+                    expect(body).toEqual({
+                        message: "Path not found"
+                    });
+                })
+        });
+    });
+});
--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const { getTopics } = require('./controllers/topics.controller');
 
 const app = express();
+
+app.get('/api/topics', getTopics);
+
+app.all('/*', (req, res) => {
+    res.status(404).send({ message: "Path not found" });
+})
 
 module.exports = app;
