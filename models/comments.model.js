23cb5d9c3c1f54f021d15403b69fdd0dc0829f58
Jonathon Coolledge <jonathon.coolledge@gmail.com>
2022-02-16 11:21:30 +0000

    Refactor selectCommentsByArticleId to not use JOIN

--- models/comments.model.js
+++ models/comments.model.js
@@ -6,8 +6,7 @@ exports.selectCommentsByArticleId = async (articleId) => {
 
     const comments = await db.query(`
         SELECT * FROM comments
-        JOIN articles ON comments.article_id = articles.article_id
-        WHERE articles.article_id = $1;
+        WHERE article_id = $1;
     `, [articleId]);
 
     return comments.rows;
